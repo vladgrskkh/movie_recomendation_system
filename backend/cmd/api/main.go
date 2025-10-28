a65de775bd6274aa7bd040a3ad45a496a7f6816e
peshkanavalnogo <playvladosik@gmail.com>
2025-10-28 14:57:47 +0300

    make: add new make rules(for building, vetting, dev help)

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -23,7 +23,10 @@ import (
 // @name Authorization
 // @description Provide a Bearer token: "Bearer {token}"
 
-const version = "1.0.0"
+var (
+	buildTime string
+	version   string
+)
 
 const (
 	LevelTrace = slog.Level(-8)
@@ -90,7 +93,14 @@ func main() {
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "MS_rGhIm9@test-r83ql3pqpxxgzw1j.mlsender.net", "SMTP sender")
 
+	displayVersion := flag.Bool("version", false, "Display version and quit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Build time:\t%s\nVersion:\t%s\n", buildTime, version)
+		os.Exit(0)
+	}
 
 	mailer := mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender)
 
@@ -151,20 +161,16 @@ func openDB(cfg config) (*sql.DB, error) {
 
 // Task for today::::::::::::::::::
 // ::::::::::::::::::::::::::::::::
-// TO DO: metrics (prometheus, grafana, expvar etc) (idk 4 house mb)
-// TO DO: makefile new rules (30 min)
 // TO DO: write tests for the handlers and other components (2 hours)
-// TO DO: routes groups (30 min)
-// TO DO: rate limiter (mb use already implemented stuff from github )(1-2 hours)
-// fix bug: mailer on vps dial i/o timeout
+// TO DO: rate limiter (mb use already implemented stuff from github)(1-2 hours)
+// fix bug: mailer on vps dial i/o timeout (fix: firewall blocks 587 so swithc to api can help)
 // ::::::::::::::::::::::::::::::::
 
 // TO DO: write tests for the handlers and other components
 // TO DO: rate limiter
-// TO DO: routes groups
 // TO DO: think about how to serve images for movies
-// TO DO: metrics (prometheus, grafana, expvar etc)
 // TO DO: python ml microservice (grpc)
 // TO DO: reset password handler
 // TO DO: user profile handler
 // bug: mailer on vps dial i/o timeout
+// TODO: add more metrics, grafana settings (best practice)
--- backend/go.mod
+++ backend/go.mod
@@ -7,6 +7,9 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/invopop/validation v0.8.0
 	github.com/lib/pq v1.10.9
+	github.com/prometheus/client_golang v1.23.2
+	github.com/swaggo/http-swagger v1.3.4
+	github.com/swaggo/swag v1.16.6
 	golang.org/x/crypto v0.43.0
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
@@ -19,7 +22,6 @@ require (
 	github.com/go-openapi/jsonpointer v0.22.1 // indirect
 	github.com/go-openapi/jsonreference v0.21.2 // indirect
 	github.com/go-openapi/spec v0.22.0 // indirect
-	github.com/go-openapi/swag v0.25.1 // indirect
 	github.com/go-openapi/swag/conv v0.25.1 // indirect
 	github.com/go-openapi/swag/jsonname v0.25.1 // indirect
 	github.com/go-openapi/swag/jsonutils v0.25.1 // indirect
@@ -27,16 +29,11 @@ require (
 	github.com/go-openapi/swag/stringutils v0.25.1 // indirect
 	github.com/go-openapi/swag/typeutils v0.25.1 // indirect
 	github.com/go-openapi/swag/yamlutils v0.25.1 // indirect
-	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.9.1 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/prometheus/client_golang v1.23.2 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.66.1 // indirect
 	github.com/prometheus/procfs v0.16.1 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
-	github.com/swaggo/http-swagger v1.3.4 // indirect
-	github.com/swaggo/swag v1.16.6 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/mod v0.29.0 // indirect
@@ -46,5 +43,4 @@ require (
 	golang.org/x/tools v0.38.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
