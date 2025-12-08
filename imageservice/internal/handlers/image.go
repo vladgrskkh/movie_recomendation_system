4fe59a0f26f6cb48cb207201837dd695ee463415
peshkanavalnogo <playvladosik@gmail.com>
2025-12-08 23:58:21 +0300

    imageservice: implemented full service and handler layers (need to improve error handling and add domain layer)

--- imageservice/internal/handlers/image.go
+++ imageservice/internal/handlers/image.go
@@ -1,1 +1,28 @@
 package handlers
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/vladgrskkh/movie_recomendation_system/imageservice/internal/service"
+
+	pb "github.com/vladgrskkh/movie-recommender-contracts/v1/imageservice"
+)
+
+type ImageHandler struct {
+	logger       *slog.Logger
+	imageService *service.ImageService
+}
+
+func NewImageHandler(logger *slog.Logger, imageService *service.ImageService) *ImageHandler {
+	return &ImageHandler{
+		logger:       logger,
+		imageService: imageService,
+	}
+}
+
+func (h *ImageHandler) Upload(ctx context.Context, r *pb.ImageUploadRequest) (*pb.ImageUploadResponse, error) {
+	image := r.GetImage()
+	err := h.imageService.UploadImage(ctx, image.GetBucketName(), image.GetObjectName(), r.GetImageBytes(), minio.PutObjectOptions{})
+}
--- imageservice/internal/service/service.go
+++ imageservice/internal/service/service.go
@@ -1,1 +1,66 @@
 package service
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/vladgrskkh/movie_recomendation_system/imageservice/internal/repository"
+)
+
+// TODO: error domain, logging
+// TODO: minio opts
+
+type ImageService struct {
+	logger         *slog.Logger
+	movieImageRepo *repository.MovieImageRepo
+}
+
+func NewImageService(movieImageRepo *repository.MovieImageRepo) *ImageService {
+	return &ImageService{
+		movieImageRepo: movieImageRepo,
+	}
+}
+
+func (s *ImageService) UploadImage(ctx context.Context, bucketName string, objectName string, object []byte, opts minio.PutObjectOptions) error {
+	reader := bytes.NewReader(object)
+
+	err := s.movieImageRepo.Upload(ctx, bucketName, objectName, reader, int64(len(object)), opts)
+	// err domain
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *ImageService) GetImage(ctx context.Context, bucketName string, objectName string, opts minio.GetObjectOptions) ([]byte, error) {
+	object, err := s.movieImageRepo.Get(ctx, bucketName, objectName, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := object.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	image := make([]byte, info.Size)
+
+	_, err = object.Read(image)
+	if err != nil {
+		return nil, err
+	}
+
+	return image, nil
+}
+
+func (s *ImageService) DeleteImage(ctx context.Context, bucketName string, objectName string, opts minio.RemoveObjectOptions) error {
+	err := s.movieImageRepo.Delete(ctx, bucketName, objectName, opts)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
